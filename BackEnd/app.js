5b089f6f19e97d733e54d04e11f4da2d8f09b88e
Eliseo-P1 <eliseomax007@gmail.com>
2025-03-09 19:13:17 -0300

    nueva-configuracion

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,14 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 import { loginUsuarioPrimero } from './src/controllers/usuario.js';
 import dotenv from "dotenv";
+
+
+
+
 
 dotenv.config(); 
 
 const puerto = process.env.PORT || 3000;
 
 const app = express();
-
-app.post('/prueba',loginUsuarioPrimero );
+app.use(cors({
+  origin: 'http://localhost:3000', // O puedes usar '*' para permitir cualquier origen
+  methods: ['GET', 'POST'], // Si es necesario, agrega más métodos permitidos
+  allowedHeaders: ['Content-Type'] // Los encabezados permitidos
+}));
+app.post('/prueba',loginUsuarioPrimero);
 
 app.listen(puerto, () => {
   console.log('Servidor escuchando en el puerto', puerto);
--- FrontEnd/src/paginas/login/login.jsx
+++ FrontEnd/src/paginas/login/login.jsx
@@ -33,6 +33,7 @@ const primerSiguiente = async ()=>{
 
 }
 
+
 const entrarLogin=()=>{
 
 }
