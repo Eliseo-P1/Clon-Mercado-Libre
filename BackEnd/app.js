1c769474077679307c4ff4b23059c360cc06a731
Eliseo-P1 <eliseomax007@gmail.com>
2025-03-08 21:06:10 -0300

    caso de prueba

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,26 +1,24 @@
+
 import express from 'express';
 import pool from "./src/configuracion/db.js";
 import dotenv from "dotenv";
+
+dotenv.config(); 
+
 const puerto = process.env.PORT || 3000;
-
-dotenv.config();
-
 
 const app = express();
 
-
-app.get('./prueba',async (req,res)=>{
-
-  const [respuesta] = await pool.query('SELECT * FROM usuario');
-
-  const usuarios = respuesta[0];
- console.log(usuarios)
-  return res.json({respuesta:usuarios})
- 
-
-})
-
-
+app.get('/prueba', async (req, res) => {
+  try {
+    const [usuarios] = await pool.query('SELECT * FROM usuario');  
+    console.log(usuarios);
+    return res.json({ respuesta: usuarios });
+  } catch (error) {
+    console.error("Error en la consulta:", error);
+    return res.status(500).json({ error: "Error en el servidor" });
+  }
+});
 
 app.listen(puerto, () => {
   console.log('Servidor escuchando en el puerto', puerto);
