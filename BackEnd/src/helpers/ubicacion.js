b4d571107e2addeefa0425cd6e55552eae2c4d73
Eliseo-P1 <eliseomax007@gmail.com>
2025-03-10 19:58:33 -0300

    probando-rut664

--- BackEnd/src/helpers/ubicacion.js
+++ BackEnd/src/helpers/ubicacion.js
@@ -3,11 +3,11 @@ import { response } from "express";
 
 export const localizador = async () => {
     try {
-        const response = await fetch('http://ip-api.com/json/');
+        const response = await fetch('https://geolocation-db.com/json/');
         if (!response.ok) throw new Error('Error en la respuesta del servidor');
 
         const data = await response.json();
-        console.log('País:', data.country); 
+        console.log('País:', data.country_name); 
         return data.country; // Devuelve el país
     } catch (err) {
         console.error('Error al obtener el país:', err);
