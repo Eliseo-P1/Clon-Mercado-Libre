91527cd18e441130ee3c4ecd76e5e643c51516e1
Eliseo-P1 <eliseomax007@gmail.com>
2025-03-17 11:17:11 -0300

    core-3

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'http://localhost:3000', 
+    origin: 'http://192.168.100.148:3000', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
